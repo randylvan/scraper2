3027e7ca328db388a4bb89e8e961944e1d04c87b
Randy Van <vanlrandy@gmail.com>
2022-01-03 18:40:55 -0700

    Initial commit

---
+++ lib/scraper2.ex
@@ -0,0 +1,18 @@
+defmodule Scraper2 do
+  @moduledoc """
+  Documentation for `Scraper2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scraper2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scraper2_test.exs
@@ -0,0 +1,8 @@
+defmodule Scraper2Test do
+  use ExUnit.Case
+  doctest Scraper2
+
+  test "greets the world" do
+    assert Scraper2.hello() == :world
+  end
+end
